0b1858d135a2deaa5eb639bd55e3d37d1a525fe9
JTechGaming <90555739+JTechGaming@users.noreply.github.com>
2022-11-02 08:21:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var navLinks = document.getElementById("navLinks");
 var support = document.getElementById('contact-support-btn');
+var scrolldown = document.getElementById('scroll-down-btn');
 
 function delay(time) {
     return new Promise(resolve => setTimeout(resolve, time));
@@ -19,3 +20,22 @@ if(support != null) {
         window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
     });
 }
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
